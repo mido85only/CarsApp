248c81fc9e52a8ba745c1c7eeb968751ff43ecb1
Mo Aziz <mido85only@gmail.com>
2024-02-18 22:23:40 +0200

    Initial Commit

---
+++ CarsApp/CarsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsAppApp.swift
+//  CarsApp
+//
+//  Created by mohammed on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
